7738e97a5627db73006b07b54092fcda74e649a1
Mahmudul-Hasan-dev <Contact.mahmudul.h@gmail.com>
2023-03-03 17:24:22 +0600

    done

---
+++ multi-stage-condition.js
@@ -0,0 +1,17 @@
+var money = 100;
+var danishPrice = 45;
+var butterBread = 35;
+var toastBiscuit = 20;
+
+if (danishPrice < money) {
+    console.log('danish khao');
+}
+else if (butterBread < money) {
+    console.log('butter bread khao');
+}
+else if (toastBiscuit < money) {
+    console.log('toast khao');
+}
+else {
+    console.log('cha khao')
+}
---
+++ nested-if.js
@@ -0,0 +1,10 @@
+var math = true;
+var geometry = true;
+if (math == true) {
+    if (geometry == true) {
+        console.log('solve')
+    }
+    else {
+        console.log('learn the math')
+    }
+}
